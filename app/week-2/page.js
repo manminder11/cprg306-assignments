4a03436e0bfec32a2d5ac04bb9c471279db01a7d
Manminder <ManminderSingh@edu.sait.ca>
2024-09-12 19:06:57 -0600

    step 3 and 4rendering of student info and  editing of root page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+import StudentInfo from "./student-info.js";
+
 export default function Page() {
   return (
     <main>
       <h1>Shopping List</h1>
+        <StudentInfo />
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Manminder Singh</h1>
+      <Link href="https://github.com/manminder11/cprg306-assignments.git">https://github.com</Link>
+    </main>
+  );
+}
