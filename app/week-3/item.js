b1714c6fa515d40d5425d17a6062f355c27a237d
Manminder <ManminderSingh@edu.sait.ca>
2024-09-17 13:11:47 -0600

    AGAIN

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,12 +1,3 @@
-export default function Item(props) {
-  return (
-    <li>
-      <h1>Name: {props.name}</h1>
-      <p>Quantity: {props.quantity}</p>
-      <p>Category: {props.category}</p>
-    </li>
-  );
-}
 
 
   
