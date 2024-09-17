88c5fdebb9439cbbd7f2c97e9fbb8efb08986e1e
Manminder <ManminderSingh@edu.sait.ca>
2024-09-17 13:00:55 -0600

    REDOING

---
+++ app/week-3/item-list.js
@@ -0,0 +1,77 @@
+export default function ItemList() {
+    return 
+        
+ const item1 = {
+  name: "milk, 4 L 🥛",
+  quantity: 1,
+  category: "dairy",
+};
+ 
+const item2 = {
+  name: "bread 🍞",
+  quantity: 2,
+  category: "bakery",
+};
+ 
+const item3 = {
+  name: "eggs, dozen 🥚",
+  quantity: 2,
+  category: "dairy",
+};
+ 
+const item4 = {
+  name: "bananas 🍌",
+  quantity: 6,
+  category: "produce",
+};
+ 
+const item5 = {
+  name: "broccoli 🥦",
+  quantity: 3,
+  category: "produce",
+};
+ 
+const item6 = {
+  name: "chicken breasts, 1 kg 🍗",
+  quantity: 1,
+  category: "meat",
+};
+ 
+const item7 = {
+  name: "pasta sauce 🍝",
+  quantity: 3,
+  category: "canned goods",
+};
+ 
+const item8 = {
+  name: "spaghetti, 454 g 🍝",
+  quantity: 2,
+  category: "dry goods",
+};
+ 
+const item9 = {
+  name: "toilet paper, 12 pack 🧻",
+  quantity: 1,
+  category: "household",
+};
+ 
+const item10 = {
+  name: "paper towels, 6 pack",
+  quantity: 1,
+  category: "household",
+};
+ 
+const item11 = {
+  name: "dish soap 🍽️",
+  quantity: 1,
+  category: "household",
+};
+ 
+const item12 = {
+  name: "hand soap 🧼",
+  quantity: 4,
+  category: "household",
+};
+}
+
+
---
+++ app/week-3/item.js
@@ -0,0 +1,25 @@
+export default function Item(props) {
+  return (
+    <li>
+      <h1>Name: {props.name}</h1>
+      <p>Quantity: {props.quantity}</p>
+      <p>Category: {props.category}</p>
+    </li>
+  );
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
