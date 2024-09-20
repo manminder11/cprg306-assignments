6a8881e5e1bb0042aa8718dbd9b302e5f3bbdccf
Manminder <ManminderSingh@edu.sait.ca>
2024-09-20 10:17:34 -0600

    borders are rounded now

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,9 @@ import ItemList from "./item-list.js";
 export default function Page() {
   return (
     <main>
-      <h1 className="font-extrabold text-4xl p-3 pl-5 text-yellow-300 ">Shopping List</h1>
+      <h1 className="font-extrabold text-4xl p-3 pl-5 text-yellow-300 ">
+        Shopping List
+      </h1>
       <header>
         <ItemList />
       </header>
