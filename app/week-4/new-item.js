a4203cbcbabbfc415867103b12b1b7d23c508e5d
Manminder <ManminderSingh@edu.sait.ca>
2024-09-25 11:17:01 -0600

    setup

---
+++ app/week-4/new-item.js
@@ -0,0 +1,7 @@
+export default function NewItem() {
+    return (
+        <main>
+            
+        </main>
+    );
+}
---
+++ app/week-4/page.js
@@ -0,0 +1,9 @@
+import NewItem from "./new-item";
+export default function Page() {
+    return (
+        <main>
+            <NewItem />
+
+        </main>
+    );
+    }
