7ea48663d128c0d7d38f6ea78326b1dea41f1eef
Manminder <ManminderSingh@edu.sait.ca>
2024-09-26 14:28:45 -0600

    tailwind starts here

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -14,7 +14,8 @@ export default function NewItem() {
   };
 
   return (
-    <div>
+    <div className="bg-i text-white pt-36 rounded-lg max-w-md mx-auto text-center">
+      <h1 className="font-extrabold to-violet-900 ">COUNTER PROGRAM FOR SHOPPING LIST</h1>
       <h1>Quantity: {quantity}</h1>
 
       <button disabled={increment > 20} onClick={increment}>
