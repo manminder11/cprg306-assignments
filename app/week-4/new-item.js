163f9ce960a7cf47ca7562646ea1770b89260258
Manminder <ManminderSingh@edu.sait.ca>
2024-09-27 19:22:02 -0600

    deep thinking tailwind

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -14,16 +14,28 @@ export default function NewItem() {
   };
 
   return (
-    <div className="bg-i text-white pt-36 rounded-lg max-w-md mx-auto text-center">
-      <h1 className="font-extrabold to-violet-900 ">COUNTER PROGRAM FOR SHOPPING LIST</h1>
-      <h1>Quantity: {quantity}</h1>
+    <div className="flex items-center justify-center min-h-screen bg-yellow-500">
+      <div className="bg-lime-600 shadow-lg p-8 text-center border-custom">
+        <h1 className="text-3xl font-bold text-center text-black mb-2 ">
+          COUNTER PROGRAM FOR SHOPPING LIST
+        </h1>
+        <h1 className="text-4xl font-bold text-black mb-4">Quantity: {quantity}</h1>
 
-      <button disabled={increment > 20} onClick={increment}>
-        Increment
-      </button>
-      <button disabled={decrement <= 1} onClick={decrement}>
-        Decrement
-      </button>
+        <button
+          className="font-large bg-gray-700 hover:bg-red-700 text-white font-bold p-2 px-4 rounded mr-2"
+          disabled={increment > 20}
+          onClick={increment}
+        >
+          Increment
+        </button>
+        <button
+          className="font-large font-large bg-yellow-800 hover:bg-cyan-700 text-white font-bold p-2 px-4  mt-12rounded"
+          disabled={decrement <= 1}
+          onClick={decrement}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
