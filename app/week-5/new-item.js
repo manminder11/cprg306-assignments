abf7a62e1ceceea916674134d712d795f900463e
Manminder <ManminderSingh@edu.sait.ca>
2024-10-07 16:59:11 -0600

    additions

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -95,6 +95,9 @@ export default function NewItem() {
             <option value="household">household</option>
             <option value="dry goods">dry goods</option>
             <option value="canned goods">canned goods</option>
+            <option value="frozen foods">frozen</option>
+            <option value="beverages">beverages</option>
+            <option value="snacks">snacks</option>
           </select>
 
           <div>
