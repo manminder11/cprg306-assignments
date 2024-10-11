2d179615cd0ebd776e1c015a515872b04b5fa393
Manminder <ManminderSingh@edu.sait.ca>
2024-10-11 14:55:03 -0600

    some more changes

--- app/week-6/item.js
+++ app/week-6/item.js
@@ -4,8 +4,10 @@ export default function Item(props) {
       <ul>
         <li>
           <section>
-            <h1 className="font-extrabold text-2xl pb-3">{props.name}</h1>
-            <p className="p-2 pl-4 m-4 bg-black max-w-sm rounded-3xl">
+            <h1 className="font-extrabold text-xl sm:text-2xl lg:text-3xl pb-3 p-2 sm:p-4 m-2 sm:m-4 bg-black max-w-full sm:max-w-sm rounded-xl sm:rounded-3xl">
+              {props.name}
+            </h1>
+            <p className="text-base sm:text-lg lg:text-xl">
               Buy {props.quantity} in {props.category}
             </p>
           </section>
