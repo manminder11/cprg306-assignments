4bc4773bb14ab31fdd0134d9efbe18d70dad60d7
Manminder <ManminderSingh@edu.sait.ca>
2024-10-20 17:14:25 -0600

    finally done

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -25,17 +25,18 @@ export default function ItemList() {
       }}
     >
       <section style={{ marginBottom: "20px" }}>
-        <h2 className="font-extrabold text-2xl text-white">Sort by:</h2>
+        <h2 className="font-extrabold text-2xl text-white mb-4">Sort by:</h2>
         <button
           className="font-extrabold bg-blue-500 text-white hover:cursor-pointer py-2 px-4 rounded-md mr-2 hover:bg-blue-700 transition-all duration-300"
           onClick={() => setSortBy("name")}
-          style={{ marginRight: "10px" }}
+          style={{ marginRight: "5px", padding: "10px 20px" }}
         >
           Name
         </button>
         <button
-          className="font-extrabold bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition-all duration-300"
+          className="font-extrabold bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition-all duration-300 m-auto"
           onClick={() => setSortBy("category")}
+          style={{ marginLeft: "5px", padding: "10px 20px" }}
         >
           Category
         </button>
@@ -43,6 +44,7 @@ export default function ItemList() {
       <ul
         style={{
           padding: "0",
+          margin: "0",
           listStyleType: "none",
           width: "100%",
           display: "flex",
