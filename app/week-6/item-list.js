4c0cc7b3d006a7950a0655c19f67ac1bb5341194
Manminder <ManminderSingh@edu.sait.ca>
2024-10-20 16:45:19 -0600

    more

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import items from "./items.json";
 import Item from "./item.js";
+
 export default function ItemList() {
   const [sortBy, setSortBy] = useState("name");
 
@@ -12,33 +13,19 @@ export default function ItemList() {
   if (sortBy === "name") {
     sortedItems.sort((a, b) => a.name.localeCompare(b.name));
   }
+
   return (
     <main>
       <section>
-        <h2 style={{ fontSize: "1.125rem", fontWeight: "bold" }}>Sort by:</h2>
+        <h2 className="font-extrabold text-2xl ">Sort by:</h2>
         <button
-          style={{
-            fontWeight: "800",
-            backgroundColor: "#3b82f6",
-            color: "white",
-            hover: "cursor",
-            
-            padding: "0.5rem 1rem",
-            borderRadius: "0.375rem",
-            marginRight: "0.5rem",
-          }}
+          className="font-extrabold bg-blue-500 text-white hover:cursor-pointer py-2 px-4 rounded-md mr-2"
           onClick={() => setSortBy("name")}
         >
           Name
         </button>
         <button
-          style={{
-            fontWeight: "800",
-            backgroundColor: "#3b82f6",
-            color: "white",
-            padding: "0.5rem 1rem",
-            borderRadius: "0.375rem",
-          }}
+          className="font-extrabold bg-blue-500 text-white py-2 px-4 rounded-md"
           onClick={() => setSortBy("category")}
         >
           Category
