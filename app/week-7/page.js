64ee15dab8c7e22e636f26393f49d3c42d91a6f5
Manminder <ManminderSingh@edu.sait.ca>
2024-10-21 11:26:03 -0600

    on going

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,5 +1,12 @@
+import useState from "react";
 import ItemList from "./item-list.js";
+import NewItem from "./new-item.js";
+import itemsdata from "./items.json";
 export default function Page() {
+
+
+  const [items, setItems] = useState(itemsdata);
+
   return (
     <main>
       <h1 className="font-extrabold text-4xl p-3 pl-5 text-yellow-300 ">
@@ -7,6 +14,8 @@ export default function Page() {
       </h1>
       <header>
         <ItemList />
+        <NewItem />
+
       </header>
     </main>
   );
