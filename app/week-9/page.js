5d5dd9019a6af1af9b68a2daaee648fb9070bb0e
Manminder <ManminderSingh@edu.sait.ca>
2024-11-06 10:46:11 -0700

    almost done but not protected

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,18 +1,47 @@
-"use client"; // Add this line at the top of the file
-
+"use client"; 
 import React from "react";
 import { useUserAuth } from "./_utils/auth-context";
-
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Page() {
+  const router = useRouter();
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  useEffect(() => {
+    if (typeof window !== "undefined" && !user) {
+      router.push("/week-9");
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return (
+      <div>
+        <button onClick={gitHubSignIn}>Sign In with GitHub</button>
+      </div>
+    );
+  }
 
   return (
     <div>
-      {user ? (
-        <button onClick={firebaseSignOut}>Sign Out</button>
-      ) : (
-        <button onClick={gitHubSignIn}>Sign In with GitHub</button>
-      )}
+      <button
+        onClick={() => {
+          firebaseSignOut();
+          window.location.href = "/week-9";
+        }}
+      >
+        Sign Out
+      </button>
+      <p>Welcome, {user.displayName}!</p>
+      <Link href="/week-9/Shopping-List" legacyBehavior>
+        <a onClick={(e) => {
+          if (!user) {
+            e.preventDefault();
+            alert("You must be signed in to access the Shopping List.");
+          }
+        }}>
+          Go to Shopping List
+        </a>
+      </Link>
     </div>
   );
 }
